9ceb9bda694011b72cb17e28f7bcdd84a3a114ef
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-04-29 08:20:02 -0300

    Create Exercício04.java

---
+++ EstruturaDeRepeticao/Exercício04.java
@@ -0,0 +1,53 @@
+package EstruturaDeRepeticao;
+
+import java.util.Scanner;
+
+public class Exercício04 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		int idade = 0, contador = 0, maiores = 0, maisVelho = 0, maisNovo = 0;
+		int acumulador = 0, mediaIdade = 0;
+		
+		while (contador <7) {
+			System.out.print("Digite a idade:");
+			idade = teclado.nextInt();
+			
+			if(idade >= 18) {
+				
+				maiores++;
+			}
+			
+			if(idade > maisVelho) {
+				
+				maisVelho = idade;
+			}
+			
+			if (contador == 0) {
+				
+				maisNovo = idade;
+			}
+			
+			if (idade < maisNovo) {
+				
+				maisNovo = idade;
+			}
+			
+			acumulador = acumulador + idade;
+			contador++;
+		}
+		
+		mediaIdade = acumulador / contador;
+	
+		System.out.printf("O total de maiores de idade é de %d.", maiores);
+		System.out.println("");
+		System.out.printf("A pessoa mais idosa tem %d anos.", maisVelho);
+		System.out.println("");
+		System.out.printf("A pessoa mais nova tem %d anos.", maisNovo);
+		System.out.println("");
+		System.out.printf("A média de idade foi de %d anos.", mediaIdade);
+		teclado.close();
+	}
+
+}
