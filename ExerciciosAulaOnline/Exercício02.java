0e58877d23a675e1ea0927bd87dec48508775989
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-08-25 13:23:10 -0300

    Create Exercício02.java

---
+++ ExerciciosAulaOnline/Exercício02.java
@@ -0,0 +1,23 @@
+package ExerciciosAulaOnline;
+
+import java.util.Scanner;
+
+public class Exercício02 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		int num = 0;
+		System.out.print("Digite um número inteiro: ");
+		num = teclado.nextInt();
+		
+		for(int i = num; i >= 0; i--) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		teclado.close();
+
+	}
+
+}
